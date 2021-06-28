38336b4e3f01ebc68c109589c03ca5c5423167ac
Akın Kemer <kemerakin@gmail.com>
2021-06-28 15:10:40 +0300

    getting only required files from api

--- src/api/ApiVariables.js
+++ src/api/ApiVariables.js
@@ -4,9 +4,11 @@ const restCountriesURL = "https://restcountries.eu/rest";
 
 const axiosInstance = axios.create({ baseURL: restCountriesURL });
 
-const apiVersion="v2"
+const apiVersion = "v2"
+
+const requiredFieldsFilter="?fields=name;capital;region;flag"
 
 
 export default axiosInstance;
 
-export const all=`/${apiVersion}/all`;
+export const all=`/${apiVersion}/all${requiredFieldsFilter}`;
